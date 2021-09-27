991fb2629447c9146974a8ac329356b6df05c79c
adhinugroho16 <91260025+adhinugroho16@users.noreply.github.com>
2021-09-27 14:16:12 +0700

    Add files via upload

---
+++ Praktikum_2_perhitungan_Surgery_Adhi_Nugroho_065002100015.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:19:56 2021
+
+@author: Surgery Adhi Nugroho
+NIM: 065002100015
+"""
+
+print("Membuat progam perhitungan")
+
+a = float(input('Masukan nilai A'))
+b = float(input('Masukan nilai B'))
+
+
+r = a + b
+print('Jumlah nilai A ditambah B adalah = ',r)
+s = a - b
+print('Pengurangan nilai A dikurang B adalah',s)
+t = a * b
+print('Perkalian nilai A dikali B adalah',t)
+u = a / b
+print('Pembagian nilai A dibagi B adalah',u)
+v = a ** b
+print('Hasil pangkat dari nilai A dan nilai B adalah',v)
+import math
+print('Hasil dari log A adalah =',math.log10(a))
+
+print('Sekian Terima Kasih')
+
